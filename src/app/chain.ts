0bb9ade8ea75ad749c4298004306caad0425d3d1
cobibean <cobibean777@gmail.com>
2024-12-23 17:24:59 -0800

    Update Metis RPC configuration

--- src/app/chain.ts
+++ src/app/chain.ts
@@ -1,23 +1,3 @@
 import { defineChain } from "thirdweb";
 
-export const chain = defineChain({
-    chainId: 1088,
-    rpc: ["https://andromeda.metis.io/?owner=1088"],
-    name: "Metis Andromeda",
-    chain: "METIS",
-    nativeCurrency: {
-        name: "Metis",
-        symbol: "METIS",
-        decimals: 18,
-    },
-    shortName: "metis",
-    slug: "metis-andromeda",
-    testnet: false,
-    explorers: [
-        {
-            name: "blockscout",
-            url: "https://andromeda-explorer.metis.io",
-            standard: "EIP3091",
-        },
-    ],
-});
+export const chain = defineChain(1088)
